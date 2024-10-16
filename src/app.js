c6c6b7051ad4557dccf289fab7540223bd3dae06
Abhishek C Sappandi <abhishek.sappandi@kaplan.com>
2024-10-16 22:49:51 +0530

    added get put delete

--- src/app.js
+++ src/app.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const { connectDB } = require('./config/database');
 const { User } = require('./models/user');
+const req = require('express/lib/request');
 const app = express();
 
 // thios is the middleware prodided by express to convert json into JS object
 app.use(express.json());
 
+// POST for signup
 app.post('/signup', async (req, res) => {
-  console.log("🚀 ~ app.post ~ req:", req.query)
-
   // creating the new instance of the User Modal
   const user = new User(req.body);
-
   try {
     await user.save();
     res.send('user save in DB ')
@@ -22,6 +21,68 @@ app.post('/signup', async (req, res) => {
   }
 })
 
+
+// GET user by FirstName
+app.get('/user', async (req, res) => {
+  try {
+    const user = await User.findOne({firstName: req.body.firstName});
+    if(!user) {
+      res.status(404).send('user not found')
+    } else {
+      res.send(user)
+    }
+  } catch (err) {
+    res.status(404).send('something went wromg')
+  }
+})
+
+// GET Feed api - get all useers fron DB
+app.get('/feed', async (req, res) => {
+  try {
+    const user = await User.find();
+    if(!user) {
+      res.status(404).send('user not found')
+    } else {
+      res.send(user)
+    }
+  } catch (err) {
+    res.status(404).send('something went wromg')
+  }
+})
+
+// DELETE user based on id
+app.delete('/user', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.body.userId);
+    if(!user) {
+      res.send(404).send('user not found')
+    } else {
+      res.send('deleted sucessfully')
+    }
+  }
+  catch (err) {
+    res.status(404).send('error occured!');
+  }
+})
+
+// patch the user based on id
+app.patch('/user', async (req, res) => {
+  const user = req.body.userId
+  const userData = req.body
+  try {
+    const updatedUser = await User.findByIdAndUpdate(user, userData)
+    if(!updatedUser) {
+      res.send(404).send('user not found')
+    } else {
+      res.send('updated sucessfully')
+    }
+  } 
+  catch (err) { 
+    res.status(500).send('error occured')
+  }
+})
+
+// used for err handling for the whole application
 app.use('/', (err, req, res, next) => {
     res.status(500).send("something went wrong");
 })
