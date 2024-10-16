c7fcf42948938994d91a15ef186ff8c57bbbbfbf
Abhishek C Sappandi <abhishek.sappandi@kaplan.com>
2024-10-16 15:05:43 +0530

    add next call back senario

--- src/app.js
+++ src/app.js
@@ -2,14 +2,24 @@ const express = require('express');
 
 const app = express();
 
+// app.use('/user', ()=>{}, [()=>{},()=>{}], ()=>{},) // works with all array and independent
+
 app.use('/user',
   (req, res, next) => {
     console.log('response 1');
     // res.send('response 1');
     next();
-}, (req, res) => {
+}, (req, res,next) => {
   console.log('response 2');
-  res.send('response 2');
+  // res.send('response 2');
+  next()
+},(req, res, next) => {
+  console.log('response 3');
+  // res.send('response 3');
+  next()
+},(req, res) => {
+  console.log('response 4');
+  res.send('response 4');
 })
 
 app.listen(4000, () => {
