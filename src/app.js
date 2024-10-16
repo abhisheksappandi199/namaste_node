b4427f3b3a36e3c0771dc0eaa4b5f2aa3bd370b0
Abhishek C Sappandi <abhishek.sappandi@kaplan.com>
2024-10-16 15:11:20 +0530

    next some senarios

--- src/app.js
+++ src/app.js
@@ -2,25 +2,46 @@ const express = require('express');
 
 const app = express();
 
-// app.use('/user', ()=>{}, [()=>{},()=>{}], ()=>{},) // works with all array and independent
-
 app.use('/user',
   (req, res, next) => {
     console.log('response 1');
     // res.send('response 1');
     next();
-}, (req, res,next) => {
-  console.log('response 2');
-  // res.send('response 2');
-  next()
-},(req, res, next) => {
-  console.log('response 3');
-  // res.send('response 3');
-  next()
-},(req, res) => {
-  console.log('response 4');
-  res.send('response 4');
 })
+
+app.use('/user',
+  (req, res, next) => {
+    console.log('response 2');
+    res.send('response 2');
+    // next();
+})
+
+
+
+
+
+
+
+// ----------------------------------------------------------------
+// app.use('/user', ()=>{}, [()=>{},()=>{}], ()=>{},) // works with all array and independent
+
+// app.use('/user',
+//   (req, res, next) => {
+//     console.log('response 1');
+//     // res.send('response 1');
+//     next();
+// }, (req, res,next) => {
+//   console.log('response 2');
+//   // res.send('response 2');
+//   next()
+// },(req, res, next) => {
+//   console.log('response 3');
+//   // res.send('response 3');
+//   next()
+// },(req, res) => {
+//   console.log('response 4');
+//   res.send('response 4');
+// })
 
 app.listen(4000, () => {
   console.log('server is listerning on port 4000');
